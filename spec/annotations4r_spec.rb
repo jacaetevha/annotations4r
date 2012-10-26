2f2bd5e6a3c50a78e82cafed57c34aa50e41072b
Jason Rogers <jason.rogers@samasource.org>
2012-10-26 10:19:27 -0400

    updated gems

--- spec/annotations4r_spec.rb
+++ spec/annotations4r_spec.rb
@@ -5,32 +5,33 @@ describe "Annotations4r" do
     def self.no_arg_clazz_method
       'no_arg_clazz_method'
     end
-    
+
     def self.one_arg_clazz_method one
       'one_arg_clazz_method: ' + one.to_s
     end
-    
+
     def self.two_arg_clazz_method one, two
       'two_arg_clazz_method: ' + one.to_s + ', ' + two.to_s
     end
-    
+
     def no_arg_instance_method
       'no_arg_instance_method'
     end
-    
+
     def self.one_arg_instance_method one
       'one_arg_instance_method: ' + one.to_s
     end
-    
+
     def self.two_arg_instance_method one, two
       'two_arg_instance_method: ' + one.to_s + ', ' + two.to_s
     end
   end
-    
+
   context "improper overrides" do
     it "raises ArityMismatch" do
       expects {
         Class.new(A) do
+        require 'debugger'; debugger
           include Annotations
 
           override
